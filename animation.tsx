d564a7721ced36560d0efe9fddf2bc769b67c1f5
premranjn <premrnjan@gmail.com>
2023-11-18 20:06:12 +0530

    Base World

---
+++ animation.tsx
@@ -0,0 +1,16 @@
+import { HNode, render } from "@hiber3d/hdk-react";
+import { Grid, Ground, SkyScraper } from "@hiber3d/hdk-react-components";
+import { Prefab } from "@hiber3d/hdk-react";
+import { Avatar } from "@hiber3d/hdk-react-components";
+import { Animation } from "@hiber3d/hdk-react";
+import { MaterialOnSignal } from "@hiber3d/hdk-react";
+import { ButtonSensor } from "@hiber3d/hdk-react";
+import { VideoPanel } from "@hiber3d/hdk-react-components";
+import { VisibleOnSignal } from "@hiber3d/hdk-react";
+import { AndGate } from "@hiber3d/hdk-react";
+import { getAvatarPosition } from '@hiber3d/hdk-core';
+import { getCameraPosition } from "@hiber3d/hdk-core/dist/types/src/HDK";
+import { InteractiveMediaAsset } from "@hiber3d/hdk-react-components";
+import { MediaDisplay } from "@hiber3d/hdk-react-components";
+
+
